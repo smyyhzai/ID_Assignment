44a7a1b01a4e9812d9c2731ebdff3810b36a9ea8
Sumayyah Zaini <s10223863@connect.np.edu.sg>
2021-12-01 01:00:54 +0800

    made pages responsive

---
+++ script.js
@@ -0,0 +1,39 @@
+//javascript to submit form
+/*
+function myFunction()
+{
+    document.getElementById("form1").submit();
+
+}
+
+window.onscroll = function() {headerFunction()};
+
+var menu = document.getElementById("myHeader");
+
+var sticky = menu.offsetTop;
+
+function headerFunction(){
+    if (window.pageYOffset > sticky){
+        menu.classList.add("sticky");
+    } else{
+        menu.classList.remove("sticky");
+    }
+}
+*/
+//nav-bar = myHeader
+//menu= menu-content
+//<nav class="menu" id="myHeader">
+//<ul id="menuContent"></ul>
+
+/*var myHeader = document.getElementById("myHeader");
+var menuContent = document.getElementById("menuContent");
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menuContent.offsetTop){
+        myHeader.classList.add("sticky");
+    }
+    else{
+        myHeader.classList.remove("sticky");
+    }
+}
+*/
